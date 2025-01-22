22d05d954c6b15921db3beac47c5409101db8846
Anup Gupta <anuup22.cs@gmail.com>
2025-01-22 13:00:37 +0530

    feat: add home route

---
+++ src/main/java/com/example/employee_crud/MainController.java
@@ -0,0 +1,12 @@
+package com.example.employee_crud;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Employee CRUD application!";
+    }
+}
